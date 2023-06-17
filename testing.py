eb1ddfafca53610cb296fdb9f4973093de29ec9a
Axel Angel <axel+git@vneko.ch>
2023-06-17 12:05:58 +0200

    update example testing.py

--- testing.py
+++ testing.py
@@ -5,14 +5,17 @@ from dsl import *
 star = union(( Cube(size=1, orient=sign+axis )
               for axis in 'xyz'
               for sign in '+-' )).translate(x=5)
-to_stl('out.stl', star)
+to_stl('star.stl', star)
 
 
 bounder = star.bounding_sphere(fn=50)
-to_stl('out_bounding.stl', bounder)
+to_stl('star_bounding.stl', bounder)
 
 
-smooth_cube = hull(( (Sphere(d=0.5, fn=20)
+Solid.fn=20
+d = 0.5
+rounder = Cylinder(d=d) & Cylinder(d=d).rotate(y=90)
+smooth_cube = hull(( (rounder
                       .orient(-1*np.array((x,y,z))) # ensure we fit the unit cube
                       .translate((x, y, z)))
                     for x in (-1,+1)
